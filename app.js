831eeee9e57ca43839b5832ff8ec312e095e2d57
Carol He <ch2727@nyu.edu>
2017-04-25 14:41:11 -0400

    passport-local is working

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
+
 //mongoose things
 // is the environment variable, NODE_ENV, set to PRODUCTION?
 if (process.env.NODE_ENV === 'PRODUCTION') {
@@ -81,8 +82,9 @@ mongoose.connect(dbconf);
 
 
 app.get('/', (req, res) => {
-	console.log(req.method, req.path, "-", res.statusCode);
-  if(!req.session.username){
+	console.log(req.method, req.path, "-", res.statusCode);;
+
+  if(!req.user){
     res.redirect('/login');
     console.log(req.method, req.path, "-", res.statusCode);
   }
@@ -111,33 +113,9 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate('local'), (req, res) => {
   console.log(req.method, req.path, "-", res.statusCode);
-  User.findOne({username: req.body.username}, (err, user) => {
-  if (!err && user) {
-    // compare with form password!
-    bcrypt.compare(req.body.password, user.password, function(err, auth) {
-      if(auth){
-        // assuming that user is the user retrieved from the database
-        req.session.regenerate((err1) => {
-          if (!err1) {
-						console.log(req.session);
-            req.session.username = user.username;
-            res.redirect('/');
-            console.log(req.method, req.path, "-", res.statusCode);
-          } else {
-            console.log('error');
-            res.send('an error occurred, please see the server logs for more information');
-          }
-        });
-      } else {
-        res.render('error', {message: 'incorrect password'});
-      }
-    });
-  } else {
-    res.render('error', {message: 'user does not exist'});
-  }
-  });
+  res.redirect('/');
 });
 
 app.get('/register', (req, res) => {
@@ -147,48 +125,14 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   console.log(req.method, req.path, "-", res.statusCode);
-  if(req.body.password.length < 8){
-    User.findOne({username: req.body.username}, (err, result) => {
-      if(result){
-        res.render('error', {message: 'user already exists & password must be at least 8 characters long'});
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+      if (err) {
+          return res.render('register', { account : account });
       }
-      else{
-        res.render('error', {message: 'password must be at least 8 characters long'});
-      }
-    });
-  } else {
-    User.findOne({username: req.body.username}, (err, result) => {
-      if(result){
-        res.render('error', {message: 'user already exists'});
-      }
-      else {
-        bcrypt.genSalt(10, function(err, salt) {
-          bcrypt.hash(req.body.password, salt, function(err, hash) {
-            const u = new User({
-              username: req.body.username,
-              password: hash
-            });
-            u.save((err1) => {
-              if(err1) {
-                  console.log(err1);
-              }
-            });
-            // assuming that user is the user object just saved to the database
-            req.session.regenerate((err2) => {
-              if (!err2) {
-                req.session.username = u.username;
-                res.redirect('/');
-                console.log(req.method, req.path, "-", res.statusCode);
-              } else {
-              console.log('error');
-              res.send('an error occurred, please see the server logs for more information');
-              }
-            });
-          });
-        });
-      }
-    });
-  }
+      passport.authenticate('local')(req, res, function () {
+          res.redirect('/');
+      });
+  });
 });
 
 app.get('/leaderboard', function (req, res) {
@@ -197,13 +141,8 @@ app.get('/leaderboard', function (req, res) {
 
 app.get('/logout', (req, res) => {
   console.log(req.method, req.path, "-", res.statusCode);
-  req.session.destroy(function(err) {
-		if(err){
-			console.log(err);
-		}
-    res.redirect('/');
-		console.log(req.method, req.path, "-", res.statusCode);
-  });
+  req.logout();
+  res.redirect('/');
 });
 
 // io.on('connection', function (socket) {
