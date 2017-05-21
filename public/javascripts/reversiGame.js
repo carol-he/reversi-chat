22b7ae4f557dcb3c8069aab65cfe78ba4a277c93
Carol He <ch2727@nyu.edu>
2017-05-21 00:19:56 -0700

    deploy to heroku

--- public/javascripts/reversiGame.js
+++ public/javascripts/reversiGame.js
@@ -319,7 +319,7 @@ function FinishGame(){
 }
 
 function updatePlayerInfo(score){
-  const url = 'http://linserv1.cims.nyu.edu:14394/api/gameroom/update?score=' + score + '&username=' + inSession;
+  const url = 'https://floating-scrubland-77433.herokuapp.com/api/gameroom/update?score=' + score + '&username=' + inSession;
   const req = new XMLHttpRequest();
   req.open('POST', url);
   req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
