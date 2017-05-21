fe5fc8250bbd2f1c38e7a93c350d2a6ab4e34418
Carol He <ch2727@nyu.edu>
2017-05-21 00:08:39 -0700

    raise bodyparser limit

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //   }
 // ));
 
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({limit: '1mb'}));
+
 // express static setup
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
